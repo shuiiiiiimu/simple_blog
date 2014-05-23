0257607adeb3b13f348f9a5c3b3a7982e0712ffc
zhangsm <765732980@qq.com>
2014-05-23 14:06:06 +0800

    modify style..

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+
+# ========================= init Section, Node =========================
+
+# s1 = Section.create(:name => "Ruby")
+# Node.create(:name => "Ruby",:summary => "...", :section_id => s1.id)
+# Node.create(:name => "Ruby on Rails",:summary => "...", :section_id => s1.id)
+# Node.create(:name => "Gem",:summary => "...", :section_id => s1.id)
+# s2 = Section.create(:name => "Web Development")
+# Node.create(:name => "Python",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Javascript",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "CoffeeScript",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "HAML",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "SASS",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "MongoDB",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Redis",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Git",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "MySQL",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Hadoop",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Google",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Java",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Tornado",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Linux",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Nginx",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Apache",:summary => "...", :section_id => s2.id)
+# Node.create(:name => "Cloud",:summary => "...", :section_id => s2.id)
+# s6 = Section.create(:name => "Ruby China")
+# Node.create(:name => "公告",:summary => "...", :section_id => s6.id)
+# Node.create(:name => "反馈",:summary => "...", :section_id => s6.id)
+# Node.create(:name => "开发",:summary => "...", :section_id => s6.id)
+# SiteNode.create(:name => "国内商业网站", :sort => 100)
+# SiteNode.create(:name => "国外著名网站", :sort => 99)
+# SiteNode.create(:name => "Ruby 社区网站", :sort => 98)
+# SiteNode.create(:name => "技术博客", :sort => 97)
+# SiteNode.create(:name => "Ruby 开源项目", :sort => 96)
+# SiteNode.create(:name => "国内企业", :sort => 95)
+# SiteNode.create(:name => "其他", :sort => 94)
+
